a4def22d8ba9b99d7e682f3e21075f7a8db557eb
thereconjacob <jvpokeyjake@gmail.com>
2023-04-21 12:17:38 +0100

    Do exercises 1-3

---
+++ Queue.cpp
@@ -0,0 +1,53 @@
+#include "Queue.h"
+
+Queue::Queue()
+{
+	
+}
+
+void Queue::Insert(int item)
+{
+    if (rear == size - 1)
+        rear = -1;
+    rear++;
+    data[rear] = item;
+    count++;
+}
+
+bool Queue::IsEmpty()
+{
+    return count == 0;
+}
+
+bool Queue::IsFull()
+{
+    return count == size;
+}
+
+int Queue::PeakFront()
+{
+    return data[front];
+}
+
+Queue::Queue(int size)
+{
+    data = new int[size];
+    front = 0;
+    rear = -1;
+    count = 0;
+}
+
+int Queue::Remove()
+{
+    int item = data[front];
+    front++;
+    if (front == size)
+        front = 0;
+    count--;
+    return item;
+}
+
+int Queue::Size()
+{
+    return count;
+}
---
+++ Queue.h
@@ -0,0 +1,20 @@
+#pragma once
+class Queue
+{
+private:
+	int count;
+	int* data;
+	int front;
+	int rear;
+	int size;
+
+public:
+	Queue();
+	void Insert(int item);
+	bool IsEmpty();
+	bool IsFull();
+	int PeakFront();
+	Queue(int size);
+	int Remove();
+	int Size();
+};
